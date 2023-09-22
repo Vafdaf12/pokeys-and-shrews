897871f5c6033c851b49e9bac2a3e281a737e504
Vincent <vafdaf11@gmail.com>
2023-09-22 21:01:14 +0200

    ✨Add observer class to smart contract

--- src/contract/SmartContract.cpp
+++ src/contract/SmartContract.cpp
@@ -11,8 +11,6 @@ SmartContract::SmartContract(const std::string& name, int parties)
     : m_name(name), m_votes(parties, VoteState::Negotiate),
       m_pState(new Negotiate(this)) {}
 
-SmartContract::~SmartContract() { delete m_pState; }
-
 void SmartContract::addCondition(const std::string& cond) {
     m_pState->addCondition(cond);
 }
--- src/contract/SmartContract.h
+++ src/contract/SmartContract.h
@@ -7,6 +7,11 @@ class ContractState;
 
 enum class VoteState { Negotiate, Accept, Complete };
 
+class ContractObserver {
+public:
+    virtual void notify() = 0;
+};
+
 class SmartContract {
 public:
     SmartContract(const std::string& name, int parties);
@@ -24,11 +29,21 @@ public:
 
     friend class ContractState;
 
+    void subscribe(ContractObserver* obs) { m_observers.push_back(obs); }
+    void unsubscribe(ContractObserver* obs) { std::erase(m_observers, obs); }
+
 private:
+    void setState(ContractState* s) {
+        if (s == m_pState) return;
+        m_pState = s;
+        for (auto o : m_observers)
+            o->notify();
+    }
     std::string m_name;
 
     std::vector<std::string> m_conditions;
     std::vector<VoteState> m_votes;
+    std::vector<ContractObserver*> m_observers;
 
     ContractState* m_pState;
 };
